a7633fc1edfc0f3a96c5bf9a14ff87bc02368a0f
David Whyte <davstar@hotmail.co.uk>
2022-05-16 19:04:01 +0100

    Preview has download button to save image

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DownloadsService } from '../downloads.service';
 import { ImageComponent } from '../image/image.component';
 import { PreviewService } from '../preview.service';
 
@@ -12,7 +13,7 @@ export class PreviewComponent implements OnInit {
   image: ImageComponent = {};
   massive: string = "WELCOME";
 
-  constructor(private previewService: PreviewService) { 
+  constructor(private previewService: PreviewService, private downloadsService: DownloadsService) { 
     this.previewServiceListener();
   }
 
@@ -25,6 +26,24 @@ export class PreviewComponent implements OnInit {
       this.image = res;
       this.massive = "LOADING...";
     })
+  }
+
+
+  //download image using component
+  downloadImage(image: ImageComponent){
+    this.downloadsService.downloadImage(image)
+    .subscribe(blob => {
+
+      var binaryData = [];
+      binaryData.push(blob);
+
+      const a = document.createElement('a');
+        const objectUrl = URL.createObjectURL(new Blob(binaryData, {type: "image/jpeg"}));
+        a.href = objectUrl;
+        a.download = 'image.jpg';
+        a.click();
+        URL.revokeObjectURL(objectUrl);
+      });
   }
 
 }
