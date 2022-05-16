ec4fd8c5aa41d0c4e10ecbf62faa1fdfbb30e3a5
David Whyte <davstar@hotmail.co.uk>
2022-05-16 03:07:05 +0100

    Update router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { DownloadsComponent } from './downloads/downloads.component';
 import { HomeComponent } from './home/home.component';
+import { ShareComponent } from './share/share.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'downloads', component: DownloadsComponent }
+  { path: 'downloads', component: DownloadsComponent },
+  { path: 'share', component: ShareComponent },
+  { path: 'about', component: AboutComponent }
 
 ];
 
