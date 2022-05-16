91b6192c734b20ea733861f9b5c9fa634f0a9e46
David Whyte <davstar@hotmail.co.uk>
2022-05-16 07:53:43 +0100

    Updated search service to pass interface objects

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { ImageComponent } from './image/image.component';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
+
+
+
   auth_token: string = "asasa21212....";
   data = new Subject();
   headers = new Headers();
+  private images: ImageComponent[] = [];
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -25,10 +31,24 @@ export class SearchService {
 
   public fetchSearch(term: string){
 
-    return this.httpClient.get(`https://api.unsplash.com/search/photos?page=1&query=${term}`, this.httpOptions)
-    .subscribe((res)=>{
-      this.data.next(res);
-      console.log(this.data)
+    this.httpClient.get(`https://api.unsplash.com/search/photos?page=1&query=${term}`, this.httpOptions)
+    .subscribe((res: any)=>{
+
+        for(let item of res.results) {
+
+          let newImage: ImageComponent  = {id: item.id, url: item.urls.full, name: item.user.username, description: item.description, blur: item.blur_hash};
+          this.images.push(newImage);
+        }
+
+        this.data.next(this.images);
+
+      console.log(this.data);
+      console.log(this.images)
+
+      return this.images;
+      
+
+    
     });
 
     //https://api.unsplash.com/search/photos?page=1&query=office
