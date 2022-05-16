1fe2c3801915928b49352d1f5c49fb3fe90c8196
David Whyte <davstar@hotmail.co.uk>
2022-05-16 01:59:11 +0100

    Removed generated code from app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HomeComponent } from './home/home.component';
 import { DownloadsComponent } from './downloads/downloads.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { PreviewComponent } from './preview/preview.component';
+import { ShareComponent } from './share/share.component';
+import { NavComponent } from './nav/nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +18,15 @@ import { PreviewComponent } from './preview/preview.component';
     HomeComponent,
     DownloadsComponent,
     SidebarComponent,
-    PreviewComponent
+    PreviewComponent,
+    ShareComponent,
+    NavComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
