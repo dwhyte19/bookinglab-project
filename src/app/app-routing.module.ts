47295a9f67194aeec3ae7a15d20501cc61c08577
David Whyte <davstar@hotmail.co.uk>
2022-05-17 07:33:35 +0100

    Removed unused components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { DownloadsComponent } from './downloads/downloads.component';
 import { HomeComponent } from './home/home.component';
 import { ShareComponent } from './share/share.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'downloads', component: DownloadsComponent },
-  { path: 'share', component: ShareComponent },
-  { path: 'about', component: AboutComponent }
-
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
